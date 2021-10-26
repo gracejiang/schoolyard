1b19f0028aada7e33853cb6469640e545c59fa41
Carol Li <caroldotli@gmail.com>
2021-10-25 20:48:37 -0400

    Add more styling

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,33 +3,49 @@ import React from 'react'
 
 function Profile() {
   return (
-    <div className='profile-wrapper'>
-        <div classname='profile-header'>
-            <div classname='bio-wrapper'>
-                <img src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png' classname='profile-img' height='200px' width='200px'></img>
-                <div classname="profile-bio-wrapper">
-                    <h1 classname="name-text">Carol Li</h1>
-                    <h3 classname="desc-text">SEAS 2022</h3>
-                    <h3 classname="desc-text">Major: Computer and Information Science</h3>
-                    <h3 classname="desc-text"><b>22</b> following</h3>
+    <div id="profile" className='wrapper'>
+        <div className='profile-header'>
+        <button className='settings-button'>Settings</button>
+            <div className='bio-info-wrapper'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png' className='profile-img' height='300px' width='300px'></img>
+                <div className="info-text-wrapper">
+                    <h1 className="name-text">Carol Li</h1>
+                    <h3 className="desc-text">
+                        SEAS 2022
+                        <br></br>
+                        Computer and Information Science
+                        <br></br>
+                        <b>22</b> following
+                    </h3>
                 </div>
             </div>
-            <div classname='bio'>
+            <div className='bio'>
                 <p>
                     Hi, I’m Carol. This is my short 100 word bio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec dui justo. Suspendisse pulvinar facilisis risus, et lacinia orci accumsan nec. Aenean sed odio in libero aliquet hendrerit. In vel tellus ut felis aliquet consequat.
                 </p>
             </div>
-            <button classname='settings-button'>Settings</button>
         </div>
-        <div classname='profile-schedule'>
-            <h2 classname='profile-section-text'>Schedule</h2>
+        <div className='schedule'>
+            <h2 className='profile-section-text'>Schedule</h2>
             <img src="https://miro.medium.com/max/1015/1*m4uMdohDG5maiJ0dIwqKuQ.png"></img>
         </div>
-        <div classname='profile-classes'>
-            <h2 classname='profile-section-text'>Classes</h2>
+        <div className='classes-section'>
+            <h2 className='section-text'>Classes</h2>
+            <div className='classes'>
+                {/* TODO(caroljli): make into buttons */}
+                <p>CIS 400</p>
+                <p>CIS 400</p>
+                <p>CIS 400</p>
+            </div>
         </div>
-        <div classname='profile-groups'>
-            <h2 classname='profile-section-text'>Groups</h2>
+        <div className='groups-section'>
+            <h2 className='section-text'>Groups</h2>
+            <div className='groups'>
+                {/* TODO(caroljli): make into buttons */}
+                <p>Hack4Impact</p>
+                <p>Hack4Impact</p>
+                <p>Hack4Impact</p>
+            </div>
         </div>
     </div>
   )
