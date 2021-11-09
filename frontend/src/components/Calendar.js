1e1fbc45efda30a2f6e501ddea4f7024432ce7ed
damian2221 <damien22211@gmail.com>
2021-11-08 23:51:47 -0500

    Styling and functionality of the basic calendar

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,21 +1,32 @@
 import React, { useRef } from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import FullCalendar from '@fullcalendar/react'
+import interactionPlugin from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
-import listPlugin from '@fullcalendar/list'
 import iCalendarPlugin from '@fullcalendar/icalendar'
 
 function Calendar() {
+  const calendarRef = useRef();
+  window.setTimeout(() => {
+    console.log(calendarRef.current)
+  })
   return (
     <>
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin, iCalendarPlugin]}
-        initialView="dayGridWeek"
+        ref={calendarRef}
+        plugins={[timeGridPlugin, iCalendarPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
         events={{
           url: `http://localhost:8082/calendar/ics/4c875e0e-ce5c-43ef-83c5-86d66f6236a1?accessToken=${
             encodeURIComponent(localStorage.accessToken)}`,
           format: 'ics',
         }}
+        eventMaxStack={3}
+        editable={true}
+        contentHeight={600}
+        nowIndicator={true}
+        scrollTime={"08:00:00"}
+        scrollTimeReset={false}
+        allDaySlot={false}
       />
     </>
   )
