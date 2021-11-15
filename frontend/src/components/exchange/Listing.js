6db380b378c537b5b1fd42eb6b219010b5f1ebc3
Grace Jiang <gracjiang@gmail.com>
2021-11-15 17:48:01 -0500

    added tags

--- frontend/src/components/exchange/Listing.js
+++ frontend/src/components/exchange/Listing.js
@@ -1,12 +1,13 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Badge } from "react-bootstrap";
 
 function Listing() {
   return (
     <div className="listing">
       <Card>
-        <Card.Body style={{padding: "30px"}}>
+        <Card.Body style={{ padding: "30px" }}>
           <div className="listing-their">
             <Card.Title>User1 is Offering</Card.Title>
+            <Badge pill bg="warning">Favor</Badge>{" "}
             <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
@@ -14,6 +15,8 @@ function Listing() {
           </div>
           <div className="listing-your">
             <Card.Title>User1 is Looking For</Card.Title>
+            <Badge pill bg="info">Item</Badge>{" "}
+            <Badge pill bg="success">Money</Badge>
             <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
@@ -21,9 +24,15 @@ function Listing() {
           </div>
 
           <div className="listing-buttons">
-            <Button className="listing-button" variant="outline-secondary">View Details</Button>
-            <Button className="listing-button" variant="outline-secondary">Message User1</Button>
-            <Button className="listing-button" variant="outline-primary">Exchange!</Button>
+            <Button className="listing-button" variant="outline-secondary">
+              View Details
+            </Button>
+            <Button className="listing-button" variant="outline-secondary">
+              Message User1
+            </Button>
+            <Button className="listing-button" variant="outline-primary">
+              Exchange!
+            </Button>
           </div>
         </Card.Body>
       </Card>
