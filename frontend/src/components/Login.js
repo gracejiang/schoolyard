0578bd146f6753da0fe3e3c5076c8529b7dd42e8
cx-lu <cxlu@wharton.upenn.edu>
2021-11-18 12:20:14 -0500

    Frontend for login and register

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,6 +2,10 @@ import { useHistory } from 'react-router'
 import { useEffect } from 'react'
 import { post } from '../util/rest'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Form, Button } from 'react-bootstrap'
+
 export default function Login() {
   const history = useHistory()
 
@@ -21,10 +25,22 @@ export default function Login() {
   }
 
   return (
-    <form onSubmit={e => handleLogin(e)}>
-      <input required type="email" placeholder="email"/>
-      <input required type="password" placeholder="password" />
-      <input type="submit" value="Submit" />
-    </form>
+    <div style={{ margin: '1em' }}>
+      <h1>Log In</h1>
+      <Form onSubmit={(e) => handleLogin(e)}>
+        <Form.Group className='mb-3'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type='text' placeholder='Username' />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+      </Form>
+    </div>
   )
 }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -2,6 +2,10 @@ import { useHistory } from 'react-router'
 import { useEffect } from 'react'
 import { post } from '../util/rest'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Form, Button } from 'react-bootstrap'
+
 export default function Register() {
   const history = useHistory()
 
@@ -19,11 +23,27 @@ export default function Register() {
   }
 
   return (
-    <form onSubmit={event => handleRegister(event)}>
-      <input required type="text" placeholder="username" />
-      <input required type="email" placeholder="email" />
-      <input required type="password" placeholder="password" />
-      <input type="submit" value="Register" />
-    </form>
+    <div style={{ margin: '1em' }}>
+      <h1>Register</h1>
+      <Form onSubmit={(e) => handleRegister(e)}>
+        <Form.Group className='mb-3'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type='text' placeholder='Username' />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='email' placeholder='Email' />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+      </Form>
+    </div>
   )
 }
