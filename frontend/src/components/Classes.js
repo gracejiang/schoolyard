0cf0b6e917972f871c79c11055e7a8d3eb124302
cx-lu <cxlu@wharton.upenn.edu>
2021-11-18 12:59:56 -0500

    Classes search

--- frontend/src/components/Classes.js
+++ frontend/src/components/Classes.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Card, Form } from 'react-bootstrap'
+
 const COURSES_URL =
   'https://apps.wharton.upenn.edu/reports/1443/data/?&course_dept=all&req_satisfied=all&ccp_flag=all'
 
@@ -17,14 +20,38 @@ export default function Classes() {
   }, [])
 
   const [courses, setCourses] = useState([])
+  const [search, setSearch] = useState('')
 
   return (
-    <div style={{ margin: '5em' }}>
-      {courses.map((c) => (
-        <div>
-          <p>{c.course_dept + ' ' + c.course_number + ': ' + c.course_title}</p>
-        </div>
-      ))}
+    <div style={{ margin: '1em' }}>
+      <Form>
+        <Form.Group className='mb-3'>
+          <Form.Control
+            type='text'
+            placeholder='Search for classes by department'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+        {courses
+          .filter((c) =>
+            c.course_dept.toLowerCase().includes(search.toLowerCase())
+          )
+          .slice(0, 52)
+          .map((c) => (
+            <Card style={{ width: '18rem', margin: '0.2em' }}>
+              <Card.Body>
+                <Card.Title>{c.course_dept + ' ' + c.course_number}</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  {c.course_title}
+                </Card.Subtitle>
+                {/* <Button variant='primary'>Join Class</Button> */}
+              </Card.Body>
+            </Card>
+          ))}
+      </div>
     </div>
   )
 }
