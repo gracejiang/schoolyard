703652f986392ec73b3f8d370599e3ce4555237c
damian2221 <damien22211@gmail.com>
2021-12-07 12:59:55 -0500

    Custom events form continued

--- frontend/src/components/scheduling/AddEventModal.js
+++ frontend/src/components/scheduling/AddEventModal.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 import { remove } from '../../util/rest'
 import { Modal, Form, Button, ButtonGroup, ToggleButton } from "react-bootstrap";
-import DateTimePicker from 'react-datetime-picker'
+import DateTimePicker from 'react-datetime'
 import UploadIcs from "./UploadIcs";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import Calendar from "./Calendar";
 
+import "react-datetime/css/react-datetime.css";
+
+
 function AddEventModal({show, setShow}) {
   const [isFreeBlock, setIsFreeBlock] = useState(false)
   const [isAllDay, setIsAllDay] = useState(false)
+  const [isRecurring, setIsRecurring] = useState(false)
   const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   return (
     <>
       <Modal show={show} onHide={() => setShow(false)}>
@@ -67,14 +72,38 @@ function AddEventModal({show, setShow}) {
             </Form.Group>
             {!isAllDay && (
               <>
-                <DateTimePicker value={startDate} onChange={setStartDate}/>
+                <Form.Group className="mb-3">
+                  <Form.Label>Start:</Form.Label>
+                  <DateTimePicker value={startDate} onChange={setStartDate}/>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>End:</Form.Label>
+                  <DateTimePicker value={endDate} onChange={setEndDate}/>
+                </Form.Group>
               </>
             )}
           </Form>
-          Is all day?
-          Start
-          End
-          Is recurring?
+          <Form.Group>
+            <Form.Check
+              type="checkbox"
+              id='is-recurring'
+              label='Is recurring?'
+              checked={isRecurring}
+              onChange={e => setIsRecurring(!isRecurring)}
+            />
+          </Form.Group>
+          {isRecurring && (
+            <>
+              <Form.Group className="mb-3">
+                <Form.Label>Start recurring event on:</Form.Label>
+                <DateTimePicker timeFormat={false} value={startDate} onChange={setStartDate}/>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>End recurring event on:</Form.Label>
+                <DateTimePicker timeFormat={false} value={endDate} onChange={setEndDate}/>
+              </Form.Group>
+            </>
+          )}
           Days of the week
           Recurrence start
           Recurrence end
