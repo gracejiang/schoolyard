61429f3baf88c0ef79cc8cf9b88f903659bc0ea1
cx-lu <cxlu@wharton.upenn.edu>
2022-02-02 20:27:59 -0500

    Fixed class search

--- frontend/src/components/Classes.js
+++ frontend/src/components/Classes.js
@@ -2,49 +2,83 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/classes.css'
 import { Button, Card, Form } from 'react-bootstrap'
 
-const COURSES_URL = 'https://apps.wharton.upenn.edu/reports/1443/data/?&course_dept=all&req_satisfied=all&ccp_flag=all'
+const COURSES_URL =
+  'https://penncoursereview.com/api/base/2022A/search/courses/?search='
 
 export default function Classes() {
-  useEffect(() => {
-    axios
-      .get(COURSES_URL)
-      .then(res => {
-        setCourses(res.data.data)
-      })
-      .catch(err => {
-        console.log(err.message)
-      })
-  }, [])
-
   const [courses, setCourses] = useState([])
   const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const ENDPOINT = `${COURSES_URL}${query}`
+    if (query !== '') {
+      axios
+        .get(ENDPOINT)
+        .then((res) => {
+          setCourses(res.data)
+        })
+        .catch((err) => {
+          console.log(err.message)
+        })
+    }
+  }, [query])
 
   return (
-    <div style={{ margin: '1em' }}>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Control
-            type="text"
-            placeholder="Search for classes by department"
-            value={search}
-            onChange={e => setSearch(e.target.value)}
-          />
-        </Form.Group>
-      </Form>
+    <div style={{ margin: '2em' }}>
+      <div className='search-bars'>
+        <Form>
+          <Form.Group className='mb-3'>
+            <Form.Label>Search for classes</Form.Label>
+            <Form.Control
+              type='text'
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </Form.Group>
+          <Button
+            variant='success'
+            onClick={(e) => {
+              setQuery(search)
+            }}
+          >
+            Search
+          </Button>
+        </Form>
+      </div>
       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+        {courses.length == 0 && (
+          <div className='bg-light no-courses'>
+            <p className='text-muted'>
+              <em>No courses found. Please enter a search query.</em>
+            </p>
+          </div>
+        )}
         {courses
-          .filter(c => c.course_dept.toLowerCase().includes(search.toLowerCase()))
-          .slice(0, 52)
-          .map(c => (
-            <Card style={{ width: '18rem', margin: '0.2em' }}>
+          .filter((c) => c.num_sections > 0)
+          .slice(0, 24)
+          .map((c) => (
+            <Card className='class-card'>
               <Card.Body>
-                <Card.Title>{`${c.course_dept} ${c.course_number}`}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  {c.course_title}
-                </Card.Subtitle>
-                {/* <Button variant='primary'>Join Class</Button> */}
+                <div className='class-card-header'>
+                  <div>
+                    <Card.Title>
+                      <strong>{c.id.replace('-', ' ')}</strong>
+                    </Card.Title>{' '}
+                    <Card.Subtitle className='class-title'>
+                      {c.title}
+                    </Card.Subtitle>
+                  </div>
+                  <div>
+                    <Button size='sm'>Join</Button>
+                  </div>
+                </div>
+                <Card.Text className='mb-2 text-muted class-preview'>
+                  {c.description}
+                </Card.Text>
               </Card.Body>
             </Card>
           ))}
