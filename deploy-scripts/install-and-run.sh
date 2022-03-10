bf2cf6d56d737246b0ee6ae270afd7f77917addd
damian2221 <damien22211@gmail.com>
2022-03-10 01:53:01 -0500

    Fix deployment files

--- deploy-scripts/install-and-run.sh
+++ deploy-scripts/install-and-run.sh
@@ -10,6 +10,7 @@ nvm install v14.18.1
 
 cd backend
 npm install
+npx kill-port 8083 # kill the previously run backend
 PORT=8083 npm start & # port 8083 will be mapped to 8082 by Apache2's reverse proxy defined in /etc/httpd/conf/httpd.conf
 
 cd ../frontend
