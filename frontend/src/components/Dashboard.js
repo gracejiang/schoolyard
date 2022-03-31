111091bb7f0a85ab8ab41b385eb21c7fd3cd8b47
Christina Lu <christina.lu.12@gmail.com>
2022-03-31 17:25:22 -0400

    Dashboard frontend

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,39 +1,46 @@
-import '../styles/App.css'
-import React from 'react'
+import "../styles/App.css";
+import React from "react";
+import { Button, Card, Form, Alert, Modal } from "react-bootstrap";
 
 function Dashboard() {
   return (
-    <div style={{textAlign: "center", padding: "20px", fontFamily: "sans-serif"}}>
-      <h1>Schoolyard</h1>
-      <ul>
-        <li>
-          <a href='upload-ics'>Upload ICS</a>
-        </li>
-        <li>
-          <a href='profile'>Profile</a>
-        </li>
-        <li>
-          <a href='register'>Register</a>
-        </li>
-        <li>
-          <a href='login'>Login</a>
-        </li>
-        <li>
-          <a href='http://127.0.0.1:8000/'>Exchanges/Marketplace</a>
-          {/* <a href='exchange'>Exchange</a> */}
-        </li>
-        <li>
-          <a href='grouplist'>Group List</a>
-        </li>
-        <li>
-          <a href='new-group'>Create Group</a>
-        </li>
-        <li>
-          <a href='classes'>Classes</a>
-        </li>
-      </ul>
+    <div>
+      <div className="dashboard-header">
+        <h1 style={{ paddingLeft: "20px" }}>Schoolyard</h1>
+        <div className="dashboard-navbar">
+          <a href="register">
+            <Button>Register</Button>
+          </a>
+          <a href="login">
+            <Button>Login</Button>
+          </a>
+        </div>
+      </div>
+      <div className="dashboard-content">
+        <br />
+        <div className="dashboard-buttons">
+          <a href="profile">
+            <div className="dashboard-button">Profile</div>
+          </a>
+          <a href="http://127.0.0.1:8000/">
+            <div className="dashboard-button">Exchanges/Marketplace</div>
+          </a>
+          <a href="grouplist">
+            <div className="dashboard-button">Groups</div>
+          </a>
+          <a href="classes">
+            <div className="dashboard-button">Classes</div>
+          </a>
+          <a href="upload-ics">
+            <div className="dashboard-button">Upload ICS</div>
+          </a>
+          <a href="new-group">
+            <div className="dashboard-button">Create Group</div>
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
